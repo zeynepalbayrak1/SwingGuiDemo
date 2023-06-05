74689d0c99fe22dcd4c1ecac3152d572ded9ea29
zeynepalbayrak1 <albayraklarzeynep@gmail.com>
2023-06-05 03:44:47 +0300

    Initial commit

---
+++ HelloDemo.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloDemo extends JFrame{
+    private JPanel panelMain;
+    private JTextField txtName;
+    private JButton btnClick;
+public HelloDemo() {
+    btnClick.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(btnClick,"Hello " +txtName.getText());
+        }
+    });
+}
+
+    public static void main(String[] args) {
+        HelloDemo h = new HelloDemo();
+        h.setContentPane(h.panelMain);
+        h.setTitle("Hello");
+        h.setSize(300,100);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
